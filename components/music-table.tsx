91e53a3db0b306818a25ff4cb3c341afcfff4e19
n4mlz <n4mlz.dev@gmail.com>
2026-09-13 16:32:52 +0900

    :fix: 楽曲テーブルの内側余白を修正

--- components/music-table.tsx
+++ components/music-table.tsx
@@ -10,37 +10,34 @@ export default function MusicTable({ data }: { data: MusicListResponse }) {
 	return (
 		<div>
 			<Header counter={`(${data.items.length})`}>楽曲一覧</Header>
-			<div className="px-4 sm:px-6">
-				<Table
-					columnDefinitions={[
-						{
-							header: "タイトル",
-							cell: (item) => (
-								<Link href={`/musics/${encodeURIComponent(item.music.id)}`}>
-									{item.music.title}
-								</Link>
-							),
-						},
-						{
-							header: "アーティスト",
-							cell: (item) => item.music.artist,
-						},
-						{
-							header: "BPM",
-							cell: (item) => item.music.bpm,
-						},
-						{
-							header: "登録日時",
-							cell: (item) =>
-								new Date(item.music.registrationDate).toLocaleDateString(
-									"ja-JP",
-								),
-						},
-					]}
-					items={data.items}
-					empty={<span>楽曲がありません。</span>}
-				/>
-			</div>
+			<Table
+				className="px-4 sm:px-6"
+				columnDefinitions={[
+					{
+						header: "タイトル",
+						cell: (item) => (
+							<Link href={`/musics/${encodeURIComponent(item.music.id)}`}>
+								{item.music.title}
+							</Link>
+						),
+					},
+					{
+						header: "アーティスト",
+						cell: (item) => item.music.artist,
+					},
+					{
+						header: "BPM",
+						cell: (item) => item.music.bpm,
+					},
+					{
+						header: "登録日時",
+						cell: (item) =>
+							new Date(item.music.registrationDate).toLocaleDateString("ja-JP"),
+					},
+				]}
+				items={data.items}
+				empty={<span>楽曲がありません。</span>}
+			/>
 		</div>
 	);
 }
