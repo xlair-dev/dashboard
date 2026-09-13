3ede02c9bc3cabf073029462e1a2885ab0c87cbc
n4mlz <n4mlz.dev@gmail.com>
2026-09-13 16:31:13 +0900

    :bug: 楽曲テーブル全体の余白を調整

--- components/music-sheets-table.tsx
+++ components/music-sheets-table.tsx
@@ -2,7 +2,7 @@
 
 import Header from "@cloudscape-design/components/header";
 import Table from "@cloudscape-design/components/table";
-import TableCell from "@/components/table-cell";
+
 import type { Sheet } from "@/lib/api";
 
 type SheetWithLabel = Sheet & { difficultyLabel: string };
@@ -13,24 +13,19 @@ export default function MusicSheetsTable({
 	sheets: SheetWithLabel[];
 }) {
 	return (
-		<Table
-			columnDefinitions={[
-				{
-					header: <TableCell>難易度</TableCell>,
-					cell: (item) => <TableCell>{item.difficultyLabel}</TableCell>,
-				},
-				{
-					header: <TableCell>レベル</TableCell>,
-					cell: (item) => <TableCell>{item.level}</TableCell>,
-				},
-				{
-					header: <TableCell>譜面制作者</TableCell>,
-					cell: (item) => <TableCell>{item.notesDesigner}</TableCell>,
-				},
-			]}
-			items={sheets}
-			header={<Header counter={`(${sheets.length})`}>譜面一覧</Header>}
-			empty={<span>譜面がありません。</span>}
-		/>
+		<div>
+			<Header counter={`(${sheets.length})`}>譜面一覧</Header>
+			<div className="px-4 sm:px-6">
+				<Table
+					columnDefinitions={[
+						{ header: "難易度", cell: (item) => item.difficultyLabel },
+						{ header: "レベル", cell: (item) => item.level },
+						{ header: "譜面制作者", cell: (item) => item.notesDesigner },
+					]}
+					items={sheets}
+					empty={<span>譜面がありません。</span>}
+				/>
+			</div>
+		</div>
 	);
 }
