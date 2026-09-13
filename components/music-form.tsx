f46b3dcf0a03f15b9b0dc2ea8bb79e286748862e
n4mlz <n4mlz.dev@gmail.com>
2026-09-14 01:39:13 +0900

    :zap: jacket 画像の読み込みを最適化

--- components/music-form.tsx
+++ components/music-form.tsx
@@ -281,6 +281,7 @@ export default function MusicForm({
 														alt="ジャケットプレビュー"
 														width={128}
 														height={128}
+														loading="eager"
 														className="size-32 object-cover"
 														unoptimized
 													/>
