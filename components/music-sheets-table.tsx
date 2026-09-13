170dac7895bcc8a20b963add328fa76d3c18a563
n4mlz <n4mlz.dev@gmail.com>
2026-09-13 16:32:52 +0900

    :bug: 楽曲テーブルの内側余白を修正

--- components/music-sheets-table.tsx
+++ components/music-sheets-table.tsx
@@ -15,17 +15,16 @@ export default function MusicSheetsTable({
 	return (
 		<div>
 			<Header counter={`(${sheets.length})`}>譜面一覧</Header>
-			<div className="px-4 sm:px-6">
-				<Table
-					columnDefinitions={[
-						{ header: "難易度", cell: (item) => item.difficultyLabel },
-						{ header: "レベル", cell: (item) => item.level },
-						{ header: "譜面制作者", cell: (item) => item.notesDesigner },
-					]}
-					items={sheets}
-					empty={<span>譜面がありません。</span>}
-				/>
-			</div>
+			<Table
+				className="px-4 sm:px-6"
+				columnDefinitions={[
+					{ header: "難易度", cell: (item) => item.difficultyLabel },
+					{ header: "レベル", cell: (item) => item.level },
+					{ header: "譜面制作者", cell: (item) => item.notesDesigner },
+				]}
+				items={sheets}
+				empty={<span>譜面がありません。</span>}
+			/>
 		</div>
 	);
 }
