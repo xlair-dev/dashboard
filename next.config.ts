20aa7157697787d82b4b3cf069d49fc7f859e3e5
n4mlz <n4mlz.dev@gmail.com>
2026-09-14 16:38:01 +0900

    :bug: proxy のアップロードサイズ上限を調整

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	output: "standalone",
 	experimental: {
+		proxyClientMaxBodySize: "35mb",
 		serverActions: {
 			bodySizeLimit: "35mb",
 		},
