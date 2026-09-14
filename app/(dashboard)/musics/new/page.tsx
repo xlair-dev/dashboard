a412a703aac36cba4fa1a0b955d87cbbc15be90a
n4mlz <n4mlz.dev@gmail.com>
2026-09-15 04:23:23 +0900

    :lock: dashboard 全体を認証必須にする

--- app/(dashboard)/musics/new/page.tsx
+++ app/(dashboard)/musics/new/page.tsx
@@ -1,7 +1,5 @@
 import MusicForm from "@/components/music-form";
-import { ensureAuthenticated } from "@/lib/api";
 
 export default async function NewMusicPage() {
-	await ensureAuthenticated("/musics/new");
 	return <MusicForm title="楽曲を追加" />;
 }
