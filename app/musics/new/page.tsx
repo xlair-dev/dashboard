2d440185507babcf1ff417de18ef0451eb4e00e6
n4mlz <n4mlz.dev@gmail.com>
2026-09-15 01:01:28 +0900

    :lock: 楽曲作成画面で事前認証を行う

--- app/musics/new/page.tsx
+++ app/musics/new/page.tsx
@@ -1,5 +1,7 @@
 import MusicForm from "@/components/music-form";
+import { ensureAuthenticated } from "@/lib/api";
 
-export default function NewMusicPage() {
+export default async function NewMusicPage() {
+	await ensureAuthenticated("/musics/new");
 	return <MusicForm title="楽曲を追加" />;
 }
